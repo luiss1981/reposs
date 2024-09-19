8d89a509e7c80849d8ea4807ac69d03ea34c4998
luisVega <luis.vega@xanelum.com>
2024-09-19 19:18:42 +0200

    test add

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
+import unittest
 from app import sumar
 
 
-def test_sumar():
-    assert sumar(1, 2) == 3
-    assert sumar(2, 3) == 5
+
+class  TestFunctions(unittest.TestCase):
+    def test_sumar_function(self):
+        self.assertEqual(sumar(1, 2), 3)
+        self.assertEqual(sumar(1, 3), 4)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
