b83132a207012a5cd4f88a3a891e7c55e5402bc8
luisVega <luis.vega@xanelum.com>
2024-09-19 21:45:11 +0200

    test add

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 def sumar(a, b):
     return a + b
+
+
+def saludo():
+    return 'Hola desde python'
+
+
+
+def execute_main():
+    print(saludo())
+
+
+
+if __name__ == '__main__':
+   execute_main()
